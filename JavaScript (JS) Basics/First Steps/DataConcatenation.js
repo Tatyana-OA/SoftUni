dcd7b8ae40a6bc23e620305d830033961d862c99
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-01-13 11:22:53 +0200

    Create DataConcatenation.js

---
+++ JavaScript (JS) Basics/First Steps/DataConcatenation.js
@@ -0,0 +1,9 @@
+function concatenate(fname,lname,age,town) {
+    let yearsofAge= Number(age);
+
+    console.log("You are "+ fname + " " + lname + ", a " + yearsofAge + "-years old person from " + town + ".")
+   //  console.log (`You are ${fname} ${lname}, a ${yearsofAge}-years old person from ${town}.`)
+    
+}
+
+concatenate('Maria', 'Ivanova', 20, 'Sofia');
