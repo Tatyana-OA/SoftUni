b439a3df537da9b6114c9dcfa9dfe5db626dcbff
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-01-17 15:29:42 +0200

    Create ArchitectProjectTiming.js
    Calculates the time needed by one architect to complete a project

---
+++ JavaScript (JS) Basics/First Steps/ArchitectProjectTiming.js
@@ -0,0 +1,7 @@
+function projects (architect, projectCount){
+    let hoursTotal = 3*projectCount;
+
+    console.log(`The architect ${architect} will need ${hoursTotal} hours to complete ${projectCount} project/s.`)
+}
+
+projects("George","4")
