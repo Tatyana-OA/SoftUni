fe3f05a2332e6aeef5356ac6fcbbb1d8d14af083
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-01-20 08:54:03 +0200

    Create GreaterNumber.js
    Determines which of two numbers is greater

---
+++ JavaScript (JS) Basics/Logical Statements/GreaterNumber.js
@@ -0,0 +1,11 @@
+function greaterNumber (num1, num2) {
+    numberFirst = Number(num1);
+    numberSecond = Number(num2);
+
+    if (numberFirst>numberSecond) {
+        console.log (numberFirst)
+    } else {
+        console.log (numberSecond)
+    }
+}
+greaterNumber("-5", "5")
