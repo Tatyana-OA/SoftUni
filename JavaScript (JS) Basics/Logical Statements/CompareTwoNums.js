7acaa50a708d42ac406444640716af2d4876e47a
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-01-21 10:35:06 +0200

    Create CompareTwoNums.js
    Simple JS logic to compare two numbers

---
+++ JavaScript (JS) Basics/Logical Statements/CompareTwoNums.js
@@ -0,0 +1,14 @@
+function solve () {
+    // Boolean values - > true/false
+
+    let firstNum=3;
+    let secondNum = 3; // '3' - string, so === will give 'False" but == will give TRUE
+    let isBigger = firstNum > secondNum;
+    
+    if (isBigger) {
+        console.log("First number is smaller than or equal to the second number")
+    }
+    
+    
+}
+solve ();
