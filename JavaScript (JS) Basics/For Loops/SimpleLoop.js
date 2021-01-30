6529e07a327b1ada3fa8071ad48c366d70c99bf8
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-01-30 08:19:43 +0200

    Create SimpleLoop.js
    Testing the index functionality of a for-loop

---
+++ JavaScript (JS) Basics/For Loops/SimpleLoop.js
@@ -0,0 +1,8 @@
+function solve (word) {
+
+    for (let index=0; index<word.length; index++) { // (word.length) е с 1 по-голямо от броя символи (index)
+        console.log (word[index]) // изпиши думата, разделяйки я на позиции (индекси)
+    }
+}
+
+solve ("hello")
