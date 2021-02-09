bc5e7d45200c02b5fe20c9139ac86c948cfcdf57
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-02-09 11:00:30 +0200

    Create 2k+1 Sequence.js
    Using while-loop to display all sequential numbers using the formula 2*k + 1 untill a given input

---
+++ JavaScript (JS) Basics/While-loops/2k+1 Sequence.js
@@ -0,0 +1,17 @@
+function solve (input) {
+    let firstnum=Number(input[0]); 
+    let start = 1; 
+    while (start<=firstnum) { 
+        console.log(start)
+    start=start*2+1; 
+    
+
+    }
+}
+
+// for (let start=1; start <= firstnum; start = 2*start + 1) 
+
+
+
+
+solve (["3"])
