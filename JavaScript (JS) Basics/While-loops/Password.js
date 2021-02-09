f306236fe039d5d2e7124520bc20ec26f020e70e
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-02-09 10:57:45 +0200

    Create Password.js
    Using two input parameters as valid user info and looping through password log attempts to check if password is valid

---
+++ JavaScript (JS) Basics/While-loops/Password.js
@@ -0,0 +1,14 @@
+function solve (input) {
+    let userName= input[0]; 
+    let password = input[1]; 
+    let data = input[2]; 
+    let index= 3 
+
+    while (data!==password) {
+        data = input[index];
+        index++; 
+        
+    }
+    console.log (`Welcome ${userName}!`)
+
+}
