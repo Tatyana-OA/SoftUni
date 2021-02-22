8f377f7333cc1ee150ba49a408d3a0e6bdbbe283
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-02-22 08:46:56 +0200

    Create ExcellentGrade.js
    Code determines whether input grade is excellent or not

---
+++ JavaScript (JS) Fundamentals/ExcellentGrade.js
@@ -0,0 +1,11 @@
+function solve (input) {
+    let grade = Number(input);
+    if (grade<5.50) {
+        console.log ("Not Excellent");
+    }
+    else {
+        console.log ("Excellent")
+    }
+}
+
+solve(3)
