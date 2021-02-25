a5da6bf4728998f65136c2b2f5b8d8b36fd6b58c
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-02-25 08:57:07 +0200

    Create StudentInfo.js
    Program logs on the console the student info (given as input) in a certain format

---
+++ JavaScript (JS) Fundamentals/Basic Syntax, Conditions, Loops/StudentInfo.js
@@ -0,0 +1,9 @@
+function solve (name, age, grade){
+    let ageS=Number(age);
+    let gradeS=Number(grade);
+
+
+    console.log (`Name: ${name}, Age: ${ageS}, Grade: ${gradeS.toFixed(2)}`)
+}
+
+solve ("John", 10, 5.6666)
