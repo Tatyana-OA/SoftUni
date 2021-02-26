567e0df5ecae96f9482cba86af8a94fae9967b1b
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-02-26 05:01:17 +0200

    Create MultiplicationTable.js
    Using for-loop to print out the multiplication table for the input number.

---
+++ JavaScript (JS) Fundamentals/Basic Syntax, Conditions, Loops/MultiplicationTable.js
@@ -0,0 +1,10 @@
+function solve (input) {
+    let num = Number(input);
+    let sum = 0
+    for (let i=1; i<=10; i++) {
+        sum = num*i;
+        console.log (`${num} X ${i} = ${sum}`)
+    }
+}
+
+solve (2)
