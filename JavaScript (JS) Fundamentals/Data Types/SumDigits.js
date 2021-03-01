09a7548ac53cb3d8cf43a08e78111a12a401862d
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-03-01 08:55:32 +0200

    Create SumDigits.js
    Program loops through the digits of an input  number and sums them

---
+++ JavaScript (JS) Fundamentals/Data Types/SumDigits.js
@@ -0,0 +1,13 @@
+function solve(input) {
+    let string = input.toString(); // от число към стринг
+    let sum = 0
+
+    for (let i= 0; i<string.length; i++) { // обхождане на стринг
+        let character = Number(string[i]) // всяко число от поредицата (въртим до дължината)
+   
+        sum += character;
+       
+
+  
+}  console.log (sum)
+} solve (543)
