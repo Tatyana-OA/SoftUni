8da68aa04f9816968988f1d1198509d0c3c7a1f2
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-03-02 09:16:12 +0200

    Create TownInfo.js
    Program logs input information about Town, population and area

---
+++ JavaScript (JS) Fundamentals/Data Types/TownInfo.js
@@ -0,0 +1,8 @@
+function solve (town, one, two) {
+    let population =  Number(one);
+    let area = Number(two);
+    console.log (`Town ${town} has population of ${population} and area ${area} square km.`)
+    
+}
+
+solve('Plovdiv',20000,250)
