44f6322122aa28f51fd999ed9ea15af6398474ed
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-03-04 10:22:49 +0200

    Create LowerOrUpper.js
    Program receives a character and determines whether it is in upper or lower case.

---
+++ JavaScript (JS) Fundamentals/Data Types/LowerOrUpper.js
@@ -0,0 +1,11 @@
+function solve (letter){
+    let lowerCase = letter.toLowerCase();
+    let UpperCase = letter.toUpperCase();
+
+    if (letter===lowerCase) {
+        console.log("lower-case")
+    } else {
+        console.log("upper-case")
+    }
+} solve ('L')
+solve ('f')
