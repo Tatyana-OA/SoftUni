b582517e93b679ab4918d19bc658f453247440c5
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-03-08 08:43:30 +0200

    Create DayOfTheWeek.js
    Using arrays to  display day of  week based on input number

---
+++ JavaScript (JS) Fundamentals/Arrays/DayOfTheWeek.js
@@ -0,0 +1,19 @@
+function solve(arr) {
+    let days = [
+      `Monday`,
+      `Tuesday`,
+      `Wednesday`,
+      `Thursday`,
+      `Friday`,
+      `Saturday`,
+      `Sunday`,
+    ];
+    if (arr <= 0 || arr >= 8) {
+      console.log(`Invalid day!`);
+    } else {
+      console.log(days[arr - 1]);
+    }
+  }
+  solve(3)
+solve(6)
+solve(11)
