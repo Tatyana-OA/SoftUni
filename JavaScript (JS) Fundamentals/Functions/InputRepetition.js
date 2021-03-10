b069259a230ff3dd88a50bf599613c5909625f0a
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-03-10 09:28:42 +0200

    Create InputRepetition.js
    Program receives 2 input parameters - a string and a number, indicating how many times that string should be repeated. The result is logged on the console.

---
+++ JavaScript (JS) Fundamentals/Functions/InputRepetition.js
@@ -0,0 +1,9 @@
+function solve (input,repetitions){
+    let result = '';
+    for (let i=1; i<=repetitions; i++) {
+        result +=input;
+        
+    }
+    console.log(result)
+}solve ("hoot",2
+    )
