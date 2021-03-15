39bba566fc441ba6493ddf64ef092ddcc33b6dac
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-03-15 08:01:49 +0200

    Create NtoNMatrix.js
    based on input number, program prints on the console a matrix with the corresponding length and that many rows

---
+++ JavaScript (JS) Fundamentals/Functions/NtoNMatrix.js
@@ -0,0 +1,14 @@
+function solve (n) {
+    let num = Number(n);
+    let result = '';
+    for (let i=0; i<num; i++) {
+        result += num;
+        result += ' '
+
+    } 
+    for (let i=0; i<num; i++) {
+        console.log (result)
+    }
+
+}
+solve (7)
