7be257d0cc19eec0a5f6b0a8ccae61ab2ccee5d1
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-03-18 09:40:28 +0200

    Create ListOfProducts.js
    Program receives an array of products. Output is printed in a way so that a numbered array of all the products ordered by name comes as a result

---
+++ JavaScript (JS) Fundamentals/Arrays Advanced/ListOfProducts.js
@@ -0,0 +1,8 @@
+function products (arr) {
+    let sorted = arr.sort();
+    for (let i=0; i<sorted.length; i++) {
+        console.log(`${i+1}.${sorted[i]}`)
+    }
+
+}
+products(["Potatoes", "Tomatoes", "Onions", "Apples"])
