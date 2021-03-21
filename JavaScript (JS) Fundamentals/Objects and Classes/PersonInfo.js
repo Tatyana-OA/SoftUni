2fac0df1aa2130266391f505534844cee6d4d1b1
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-03-21 08:52:29 +0200

    Create PersonInfo.js
    A function that receives 3 parameters, sets them to an object and returns that object.
    The input comes as 3 separate strings in the following order: firstName, lastName, age.

---
+++ JavaScript (JS) Fundamentals/Objects and Classes/PersonInfo.js
@@ -0,0 +1,10 @@
+function personInfo(firstName, lastName, age){
+let person = {} // empty object
+person.firstName=firstName;
+person.lastName=lastName;
+person.age=age;
+// fill in the empty object with key + value from the input
+
+return person;
+
+}
