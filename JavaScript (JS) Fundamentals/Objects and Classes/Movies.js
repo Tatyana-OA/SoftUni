37f8505046b1919e86e64f198cd25ade4a2ea615
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-03-24 09:44:51 +0200

    Create Movies.js
    A function that stores information about movies inside an array. The movies object info is: name, director and date. Input can be of several types:
    •	"addMovie {movie name}" – add the movie
    •	"{movie name} directedBy {director}" – check if the movie exists and then add the director
    •	"{movie name} onDate {date}" – check if the movie exists and then add the date
    At the end program prints all the movies that have all the info (if the movie has no director, name or date, don’t print it) in JSON format.

---
+++ JavaScript (JS) Fundamentals/Objects and Classes/Movies.js
@@ -0,0 +1,32 @@
+function solve(arr){
+let movies = [];
+for (const line of arr){
+    if (line.includes('addMovie')){
+    let currentMovie = {}; // обект за всеки филм
+    let name = line.substring(9); // взима стринг до 9-тия знак, т.е. "изрязва" Addmovie и space и взима натам
+    currentMovie.name = name;
+    movies.push(currentMovie)
+} else if (line.includes('directedBy')){
+let commands = line.split (' directedBy ');
+let [movie,movieDirector]=commands
+let myMovie = movies.find(x => x.name == movie); // от останалите команди намира имената, които са равни на тези, вече записани в обекти
+if (myMovie) {
+    myMovie.director = movieDirector; // при такива - добавя името на director
+}
+}
+else {
+    let [movie, movieDate]=line.split(' onDate ');
+    let myMovie = movies.find (x => x.name ==movie); // аналогично за имена с дати
+    if (myMovie){
+        myMovie.date = movieDate; //добавя датата
+    }
+}
+}
+
+movies.forEach(movie => { // минава през обекта и запазва всички, които имат и име, и режисьор, и дата
+    if(movie.name && movie.director && movie.date) {
+        console.log(JSON.stringify(movie))
+    }
+})
+
+}
