e5b8a6f24d9df98d126e6c3d4abefb00d12a2575
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-04-04 08:28:31 +0300

    Create PasswordGenerator.js
    The task here is to concatenate the first two strings and replace all vowels in the concatenated string with symbols from the third string. First vowel must be replaced with the first character from third string, second vowel with the second character from that string and so on. If the third string is less than the vowels count in the newly formed string you need to start over with character on 0 index. When you replace all vowels reverse the new password and print it on the console in a format:
     'Your generated password is {password}'

---
+++ JavaScript (JS) Fundamentals/Text Processing/PasswordGenerator.js
@@ -0,0 +1,36 @@
+function password(input){
+    let stringOne = input.shift();
+    let stringTwo = input.shift();
+    let word = input.shift().split("");
+    let wordSafe = [];
+    let myRegExp = /[ioeau]/g;
+    let concatenatedString = stringOne.concat(stringTwo).split("").join("");
+    let matchedVowel = myRegExp.exec(concatenatedString)
+    let replacedConcat = concatenatedString;
+    let counter=0;
+    while (matchedVowel!=null){
+         //i, o, e, a, u -> vowels
+        let currentVowel = matchedVowel[0]
+        let symbol = word[counter].toUpperCase();
+        replacedConcat=replacedConcat.replace(currentVowel,symbol)
+        counter++
+        if (counter==word.length){
+            counter=0;
+        }
+        matchedVowel = myRegExp.exec(concatenatedString) 
+    }
+    let reversed = replacedConcat.split("").reverse()
+    console.log(`Your generated password is ${reversed.join("")}`)
+ 
+
+    
+   
+    //replace them with symbols from word 
+    //if word lenth is shorter, start again from index 0;
+    //reverse
+
+}password([
+    'ilovepizza', 'ihatevegetables',
+    'orange'
+    ]
+    )
