ce49b1b1e8a8c71b4bbfdda8f011260a67f52cda
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-04-16 11:47:22 +0300

    Create StringLength.js
    A JS function that takes three string arguments as an input. Task is to Calculate the sum of the length of the strings and the average length of the strings rounded down to the nearest integer.
    The input comes as three string arguments passed to your function.
    The output should be printed on the console on two lines.

---
+++ JavaScript (JS) Advanced/StringLength.js
@@ -0,0 +1,7 @@
+function strlenght(input1, input2, input3) {
+let total = Number(input1.length)+Number(input2.length)+Number(input3.length);
+let avg = Math.floor(total/3)
+console.log(total)
+console.log(avg)
+} 
+strlenght('chocolate', 'ice cream', 'cake')
