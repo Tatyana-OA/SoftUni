343a13af179839c3645d24e2c09471d81d9c2d2e
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-04-27 07:33:39 +0300

    Create 3.	AddandRemoveElements.js
    A JS function that adds and removes numbers to / from an array. Two types of commands are received: "add" or "remove". 
    The initial number is 1. Each input command should increase that number, regardless of what it is.

---
+++ JavaScript (JS) Advanced/02. Arrays and Nested Arrays/3.	AddandRemoveElements.js
@@ -0,0 +1,36 @@
+function addRemove(input) {
+    let result = [];
+    let starter = 0;
+
+    for (let i=0; i <input.length; i++) {
+        let command = input[i]
+        starter++
+        if (command==='add') {      
+            result.push(starter)
+        } else if (command==='remove' && input.includes("add")) {
+            result.pop()
+        } 
+        
+    }
+    if (result.length<1) {
+        return 'Empty'
+    }else {
+        return result.join('\n')
+    }
+    
+}
+console.log(addRemove(['add', 
+'add', 
+'add', 
+'add']
+))
+console.log(addRemove(['add', 
+'add', 
+'remove', 
+'add', 
+'add']
+))
+console.log(addRemove(['remove', 
+'remove', 
+'remove']
+))
