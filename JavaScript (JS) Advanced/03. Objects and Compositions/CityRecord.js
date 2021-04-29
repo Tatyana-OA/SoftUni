7e10ab9c53d46299a67702d409d36a86f5e829f4
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-04-29 10:09:13 +0300

    Create CityRecord.js
    Received input: a city’s name (string), population (number), and treasury (number) as arguments.  Task is to set them  as properties of an object and return it.

---
+++ JavaScript (JS) Advanced/03. Objects and Compositions/CityRecord.js
@@ -0,0 +1,12 @@
+function city (name, population,treasury) {
+  return cityObj = {name,population,treasury}
+  
+}
+console.log(city('Tortuga',
+7000,
+15000
+))
+console.log(city('Santo Domingo',
+12000,
+23500
+))
