2d810646e33273a9ef6d4b72b879bfef2d017937
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-06-24 07:48:45 +0300

    Create 04. ExtendPrototype.js
    Write a function which receives a class and attaches to it a property species with value "Human" and a function toSpeciesString(). When called, the function returns a string with format:
    "I am a <species>. <toString()>"
    The function toString() is called from the current instance (call using this).
    Input / Output
    Your function will receive a class whose prototype it should extend. There is NO output, your function should only attach the properties to the given class’ prototype.

---
+++ JavaScript (JS) Advanced/09. Prototype and Inheritance/04. ExtendPrototype.js
@@ -0,0 +1,8 @@
+function extendPrototype(classToExtend) {
+
+    classToExtend.prototype.species = "Human" //adding species: Human via the prototype
+    classToExtend.prototype.toSpeciesString = function (){
+        return `I am a ${this.species}. ${this.toString()}` // using "this" which is the scope of classToExtend
+    }
+    // adding a function to a class using the prototype
+}
