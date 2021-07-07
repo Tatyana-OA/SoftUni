4bd472ccdb748c9cbb2a9c10c0dd83b6e569eaa4
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-07-07 11:49:28 +0300

    Create index.js

---
+++ JavaScript (JS) BackEnd/NoSQL & MongoDB/01. Cubicle Pt2/index.js
@@ -0,0 +1,29 @@
+
+const express = require('express');
+const expressConfig = require('./config/express');
+const databaseConfig = require('./config/database')
+const routesConfig = require('./config/routes')
+
+
+//initializing middleware created in models/storage
+const {init: storage} = require('./services/storage')
+
+start()
+
+async function start() {
+    const port = 3000 // where server will be available
+    const app = express(); // the actual express app
+   
+
+    expressConfig(app)
+    // !!! MIDDLEWARE BEFORE ROUTES
+    await databaseConfig(app) // using await because it's a Promise
+    app.use( await storage()) // -> we pack everything into an async function Start so we can await init function which is renamed to "storage"
+    routesConfig(app)
+
+    // middleware is set to be used throughout whole app
+    //setting up body-parser middleware
+
+
+app.listen(port, () => console.log(`Server listening on port ${port}`))
+}
