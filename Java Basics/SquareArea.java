5a27d3120cc404011d68faac0892865a44d2f2a5
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-07-23 13:15:22 +0300

    Create SquareArea.java

---
+++ Java Basics/SquareArea.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/**
+ * Created by tatya on 3/9/2017.
+ */
+public class SquareArea {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int n = Integer.parseInt(console.nextLine());
+        System.out.println(n*n);
+    }
+}
