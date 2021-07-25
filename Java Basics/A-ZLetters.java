898d2f53b764e21dc74ba883ed98d0a7b32820e5
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-07-25 21:20:50 +0300

    Create A-ZLetters.java

---
+++ Java Basics/A-ZLetters.java
@@ -0,0 +1,11 @@
+/**
+ * Created by tatya on 2/4/2017.
+ */
+public class A_Zletters {
+    public static void main(String[] args) {
+        for (char letter = 'a'; letter <='z'; letter++) {
+            // char letter = (char)i;
+            System.out.println(letter+ " ");
+        }
+    }
+}
