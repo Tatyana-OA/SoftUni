8fca1018354a9b0bd0ec6016c6be964074350370
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-07-26 11:22:35 +0300

    Create VowelSum.java

---
+++ Java Basics/VowelSum.java
@@ -0,0 +1,14 @@
+/**
+ * Created by tatya on 2/4/2017.
+ */
+public class Vowel_Sum {
+    public static void main(String[] args) {
+
+        int a = 1;
+
+        System.out.println(1 + ++a);
+
+        }
+    }
+
+
