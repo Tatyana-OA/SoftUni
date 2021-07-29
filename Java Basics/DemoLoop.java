0deda9bb1377c71ba5eef15f8a81c822e0879558
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-07-29 12:39:06 +0300

    Create DemoLoop.java

---
+++ Java Basics/DemoLoop.java
@@ -0,0 +1,10 @@
+/**
+ * Created by tatya on 2/4/2017.
+ */
+public class DemoCycle {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
