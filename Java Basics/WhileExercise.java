fdb04f430eecb58b67a8d46af7d57e3a46c00984
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-07-29 12:38:41 +0300

    Create WhileExercise.java

---
+++ Java Basics/WhileExercise.java
@@ -0,0 +1,13 @@
+/**
+ * Created by tatya on 2/20/2017.
+ */
+public class WhileExercise {
+    public static void main(String[] args) {
+        int a =5;
+        while (a > 0)  // while this is true, do the thing
+        {
+            System.out.println(a);
+            a--;
+        }
+    }
+}
