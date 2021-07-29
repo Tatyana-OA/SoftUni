8cdb59d8d731f08f42a3d8a8197f1e603ba522a5
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-07-29 12:39:41 +0300

    Create NumbersEndingIn7.java

---
+++ Java Basics/NumbersEndingIn7.java
@@ -0,0 +1,11 @@
+/**
+ * Created by tatya on 2/4/2017.
+         */
+public class Numbers_Ending_in_7 {
+    public static void main(String[] args) {
+        for (int i = 1; i <=1000 ; i++) {
+            if (i % 10== 7) {
+                System.out.println(i);
+            }   }
+    }
+}
