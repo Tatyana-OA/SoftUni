2a82eaa48d18a005c6c64ff5cbf35e311a9e0639
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-08-17 15:18:05 +0300

    Add files via upload
    config files and middlewares

---
+++ JavaScript (JS) BackEnd/TheatersExam/config/routes.js
@@ -0,0 +1,10 @@
+const authController = require('../controllers/authController')
+const playController = require('../controllers/playController')
+const homeController = require('../controllers/homeController')
+
+module.exports = (app) => {
+    //whenever a request starting with /auth is received, authController tackles rest
+    app.use('/auth', authController);
+	app.use('/play', playController);
+	app.use('/', homeController);
+}
